6e644e61d2ff00bcba5e07d483344f766c3281a1
Arthur Mois <76664491+arthurmois@users.noreply.github.com>
2021-06-15 01:06:06 -0700

    upload src

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+
+function App(){
+  const element = <h1>Hello, world</h1>;
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+
+
+
+
+export default App;
