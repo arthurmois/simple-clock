e01a2c18d03e31566eae9b3ddbf4161c56a0b25c
Arthur Mois <amois@ucsc.edu>
2021-06-21 20:36:45 -0700

    event handling

--- src/Clock.js
+++ src/Clock.js
@@ -5,6 +5,9 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
+
+    this.pauseTimeClick = this.pauseTimeClick.bind(this);
+    this.resumeTimeClick = this.resumeTimeClick.bind(this);
   }
 
   componentDidMount() {
@@ -24,11 +27,23 @@ class Clock extends React.Component {
     });
   }
 
+  pauseTimeClick() {
+    this.componentWillUnmount();
+  }
+
+  resumeTimeClick() {
+    this.componentDidMount();
+  }
+
 
   render() {
     return (
       <div>
         <h2>It's already {this.state.date.toLocaleTimeString()}!</h2>
+
+        <button onClick={this.pauseTimeClick} >Pause Time!</button>
+        <br/>
+        <button onClick={this.resumeTimeClick} >Resume</button>
       </div>
     );
   }
